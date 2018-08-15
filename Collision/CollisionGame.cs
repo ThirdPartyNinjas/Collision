54bd10103ab2fdf1eb75b3b023ce229001005a80
Jesse Chounard <jesse@thirdpartyninjas.com>
2018-08-15 15:22:23 -0500

    Created projects and added dependencies (FNA and fnalibs)

---
+++ Collision/CollisionGame.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Collision
+{
+    public class CollisionGame : Game
+    {
+        public CollisionGame()
+        {
+            IsFixedTimeStep = false;
+            IsMouseVisible = true;
+
+            var graphicsDeviceManager = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = 1280,
+                PreferredBackBufferHeight = 720,
+            };
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+        }
+    }
+}
---
+++ Collision/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Collision
+{
+    internal class Program
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetDllDirectory(string lpPathName);
+
+        private static void Main(string[] args)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                SetDllDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Environment.Is64BitProcess ? "x64" : "x86"));
+            }
+
+            using (CollisionGame game = new CollisionGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
